70a0d6386ff322061c4a1d5b84492ccd34443d02
Maggie Love <maggie.love1991@gmail.com>
2024-05-21 22:02:39 -0400

    Customizes footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,24 +3,30 @@ import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="border-t border-black">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
+        <div className="py-8 flex flex-col lg:flex-row items-center justify-between">
+          <div className="flex lg:text-left lg:w-2/3">
+            <ul className="text-2xl font-bold tracking-tighter leading-tight text-center mb-10 lg:mb-0 lg:pr-4 lg:w-1/3">
+              <li className="text-left">Contact</li>
+              <li className="text-left">Events</li>
+              <li className="text-left">Donate</li>
+            </ul>
+            <div className="flex flex-col justify-center items-center lg:pl-4">
+              <p className="text-2xl font-bold">Follow Us</p>
+              <ul>
+                <li className="text-2xl"><a href="">X</a></li>
+                <li className="text-2xl" a href="">Instagram</li>
+              </ul>
+            </div>
+          </div>
+          <div>
             <a
               href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fb%2Fb0%2FThe_University_of_California_Irvine.svg%2F512px-The_University_of_California_Irvine.svg.png&f=1&nofb=1&ipt=73fd7748658c492de5d98ba34c41bca6da2a759a9f696e90eb1ce93e209f47b6&ipo=images"
+              alt="UC Irvine logo" className="w-36 h-36" />
             </a>
           </div>
         </div>
